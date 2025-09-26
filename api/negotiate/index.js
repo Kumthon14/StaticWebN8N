4fedbf29ced0fd84f6b8a9af3fe63fdef9aace46
kumthon <itznongdong@gmail.com>
2025-09-27 00:40:17 +0700

    again

--- api/negotiate/index.js
+++ api/negotiate/index.js
@@ -1,21 +1,3 @@
-const { WebPubSubServiceClient } = require("@azure/web-pubsub");
-
-module.exports = async function (context, req) {
-    // ดึง Connection String ของ SignalR และ Storage จาก Environment Variables
-    const signalRConnectionString = process.env.SIGNALR_CONNECTION_STRING;
-    const hubName = "summarizer"; // ตั้งชื่อ Hub ของเรา
-
-    if (!signalRConnectionString) {
-        context.res = { status: 500, body: "AzureSignalRConnectionString is not set." };
-        return;
-    }
-
-    let serviceClient = new WebPubSubServiceClient(signalRConnectionString, hubName);
-    let token = await serviceClient.getClientAccessToken();
-
-    context.res = {
-        body: {
-            url: token.url
-        }
-    };
+module.exports = async function (context, req, connectionInfo) {
+    context.res.body = connectionInfo;
 };
